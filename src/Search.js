3bf3be4eb7be96ae004d03943f3d13131eeca0d3
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-11-18 20:11:16 -0600

    Integrated Dictionary API

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Search(props) {
   let [newWord, setNewWord] = useState(props.defaultWord);
 
-  function searchWord(event) {
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  function displayWord(event) {
     event.preventDefault();
-    alert([newWord]);
+    alert(`Searching for ${newWord}...`);
+    findWord();
   }
   function updateWord(event) {
     setNewWord(event.target.value);
   }
+  function findWord() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${newWord}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
   return (
     <div className="search">
-      <form onSubmit={searchWord}>
-        <div class="input-group mb-2">
+      <form onSubmit={displayWord}>
+        <div className="input-group mb-2">
           <input
             type="search"
             className="form-control"
@@ -23,7 +33,7 @@ export default function Search(props) {
             onChange={updateWord}
           />
           <button
-            class="btn btn-outline-secondary"
+            className="btn btn-outline-secondary"
             type="submit"
             id="button-addon2"
           >
